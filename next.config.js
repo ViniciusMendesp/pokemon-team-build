f38d9b689bb31fdfd7e85373ab5b1e361ec9951c
ViniciusMendesp <vinimendes2021@hotmail.com>
2023-02-27 08:07:54 -0300

    config next and vercel

--- next.config.js
+++ next.config.js
@@ -4,24 +4,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true
   },
-   // Comandos personalizados de construção aqui
-  //  async rewrites() {
-  //   console.log("Instalando as dependências do projeto...")
-  // //   await require('child_process').execSync('npm install')
-  // //   console.log("Dependências instaladas.")
-
-  // //   console.log("Executando o comando 'npm run build'...")
-  // //   await require('child_process').execSync('npm run build')
-  // //   console.log("Construção concluída.")
-
-  // //   console.log("Executando o comando 'npm run db:migrate:prod'...")
-  // //   await require('child_process').execSync('npm run db:migrate:prod')
-  // //   console.log("Migração do banco de dados concluída.")
-
-  // //   console.log("Executando o comando 'npm run start'...")
-  // //   await require('child_process').execSync('npm run start')
-  // //   console.log("Aplicativo iniciado.")
-  // }
 }
 
 module.exports = nextConfig
