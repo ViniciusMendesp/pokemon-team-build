3f7998b5e2f9e900183deb6c16c57710ff50d66c
ViniciusMendesp <vinimendes2021@hotmail.com>
2023-02-27 10:46:31 -0300

    gap

--- src/styles/pages/index.styles.ts
+++ src/styles/pages/index.styles.ts
@@ -59,7 +59,7 @@ export const ChoiceContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 25px;
+  gap: 30px;
   flex-wrap: wrap;
 
   margin-bottom: 24px;
